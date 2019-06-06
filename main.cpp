aeb5e0312e6819d8e1ac24327dde7ae1275b1f31
Ege Furkan Bilgin <furkanegebilgin@gmail.com>
2019-06-07 01:24:29 +0300

    Data to class implementation done.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <algorithm>
-#include <any>
 #include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 #include <tuple>
 #include <utility>
-#include <variant>
 #include <vector>
 
 using DictPair = std::pair<std::string, unsigned short>;
@@ -50,13 +48,15 @@ struct CSVHandler {
 struct Assistant {
 
 	std::string name;
+	unsigned short hoursToAssist;
 	unsigned short maxCourses;
 	std::vector<DictPair> assistedCourses;
 
 	Assistant(const std::vector<std::string> &args) {
 		name = args[0];
-		maxCourses = std::stoi(args[1]);
-		for (short i = 2; i < args.size(); ++i) {
+		hoursToAssist = std::stoi(args[1]);
+		maxCourses = std::stoi(args[2]);
+		for (short i = 3; i < args.size(); ++i) {
 			appendAssistedCourses(args[i]);
 		}
 	}
@@ -70,11 +70,10 @@ struct Assistant {
 			assistedCourses.push_back(std::make_pair(course, 1));
 		else
 			search->second++;
-		auto descending = [](DictPair &a, DictPair &b) { return a.second > b.second; };
+		auto descending = [](const DictPair &a, const DictPair &b) {
+			return a.second > b.second;
+		};
 		std::sort(assistedCourses.begin(), assistedCourses.end(), descending);
-	}
-
-	void sortAssistedCourses() {
 	}
 };
 
@@ -84,21 +83,47 @@ struct Course {
 	unsigned short minTAHours;
 	unsigned short maxTAHours;
 	unsigned short minTACount;
+
+	Course(const std::vector<std::string> &args) {
+		courseID = args[0];
+		instructorName = args[1];
+		minTAHours = std::stoi(args[2]);
+		maxTAHours = std::stoi(args[3]);
+		minTACount = std::stoi(args[4]);
+	}
 };
 
 int main(int argc, char const *argv[]) {
 	CSVHandler assistantsCSV{"../assistants.csv"};
-	auto params = assistantsCSV.read();
-	Assistant a(params[0]);
-	std::cout << a.name << " - " << a.maxCourses << std::endl;
-	std::cout << a.assistedCourses[1].first << std::endl;
-	std::cout << a.assistedCourses[1].second << std::endl;
-	std::cout << a.assistedCourses[2].first << std::endl;
-	std::cout << a.assistedCourses[2].second << std::endl;
+	CSVHandler coursesCSV{"../courses.csv"};
+	
+	auto assistantData = assistantsCSV.read();
+	auto courseData = coursesCSV.read();
+	
+	auto assistants = std::vector<Assistant>();
+	auto courses = std::vector<Course>();
 
-	for (auto fieldRow : params) {
-		for (auto field : fieldRow) {
-		}
-	}
+	for (auto dataRow : assistantData)
+		assistants.push_back(Assistant(dataRow));
+
+	auto descending = [](const Assistant &a, const Assistant &b) {
+		return a.assistedCourses.size() > b.assistedCourses.size();
+	};
+	std::sort(assistants.begin(), assistants.end(), descending);
+	
+	for (auto assistant : assistants)
+		std::cout << assistant.name << " " << assistant.assistedCourses.size() << std::endl;
+
+	for (auto dataRow : courseData)
+		courses.push_back(Course(dataRow));
+
+	auto coursesDesc = [](const Course &a, const Course &b) {
+		return a.minTAHours > b.minTAHours;
+	};
+	std::sort(courses.begin(), courses.end(), coursesDesc);
+	
+	for (auto course : courses)
+		std::cout << course.courseID << " " << course.minTAHours << std::endl;
+
 	return 0;
 }
