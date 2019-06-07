97da5f1e468774c1716f5dd3d6c81e5fb6b38e0c
Ege Furkan Bilgin <furkanegebilgin@gmail.com>
2019-06-07 21:58:43 +0300

    Implemented copy-ctor, move-ctor & move-assignment operator for Assistant class.

--- main.cpp
+++ main.cpp
@@ -10,23 +10,29 @@
 using DictPair = std::pair<std::string, unsigned short>;
 
 struct CSVHandler {
-	std::fstream inputStream;
+	std::fstream inputFile;
 
-	CSVHandler(std::string filePath) {
-		inputStream.open(filePath);
-
-		if (!inputStream.is_open()) {
+	CSVHandler(const std::string& filePath) {
+		inputFile.open(filePath);
+		if (!inputFile.is_open()) {
 			std::perror("CSVHandler constructor");
 		}
 	}
 
+	// RAII
+	~CSVHandler() {
+		inputFile.close();
+		std::cout << "RAII" << std::endl;
+	}
+
+	// Read .csv data into a 2D std::vector of std::string.
 	auto read() -> std::vector<std::vector<std::string>> {
 		std::vector<std::string> fieldRows;
 		std::vector<std::vector<std::string>> fields;
 		std::string line;
 		std::string field;
 		std::stringstream lineStream;
-		while (std::getline(inputStream, line)) {
+		while (std::getline(inputFile, line)) {
 			lineStream = std::stringstream{line};
 			while (std::getline(lineStream, field, ',')) {
 				field.erase(std::remove(field.begin(), field.end(), ' '), field.end());
@@ -37,40 +43,62 @@ struct CSVHandler {
 		}
 		return fields;
 	}
-
-	// RAII
-	~CSVHandler() {
-		inputStream.close();
-		std::cout << "RAII" << std::endl;
-	}
 };
 
 struct Assistant {
-
+	friend inline void swap(Assistant& first, Assistant& second);
 	std::string name;
 	unsigned short hoursToAssist;
 	unsigned short maxCourses;
 	std::vector<DictPair> assistedCourses;
-
-	Assistant(const std::vector<std::string> &args) {
-		name = args[0];
-		hoursToAssist = std::stoi(args[1]);
-		maxCourses = std::stoi(args[2]);
+	Assistant() = default;
+	Assistant(const std::vector<std::string>& args) : name(args[0]),
+																										hoursToAssist(std::stoi(args[1])),
+																										maxCourses(std::stoi(args[2])) {
 		for (short i = 3; i < args.size(); ++i) {
 			appendAssistedCourses(args[i]);
 		}
 	}
 
-	void appendAssistedCourses(std::string course) {
-		auto criterion = [&course](const DictPair &p) {
-			return (p.first == course);
-		};
+	// Copy constructor.
+	Assistant(const Assistant& other) : name(other.name),
+																			hoursToAssist(other.hoursToAssist),
+																			maxCourses(other.maxCourses),
+																			assistedCourses(other.assistedCourses) {}
+
+	// Move constructor.
+	Assistant(Assistant&& other) : Assistant() {
+		swap(*this, other);
+	}
+	// Copy assignment operator - commented out because it created ambiguity.
+	// Assistant& operator=(Assistant other) {
+	// 	swap(*this, other);
+	// 	return *this;
+	// }
+
+	// Move assignment operator.
+	Assistant& operator=(Assistant&& other) {
+		swap(*this, other);
+		return *this;
+	}
+
+	friend inline void swap(Assistant& first, Assistant& second) {
+		// Enable ADL.
+		// using std::swap;
+		std::swap(first.name, second.name);
+		std::swap(first.hoursToAssist, second.hoursToAssist);
+		std::swap(first.maxCourses, second.maxCourses);
+		std::swap(first.assistedCourses, second.assistedCourses);
+	}
+
+	void appendAssistedCourses(const std::string& course) {
+		auto criterion = [&course](const DictPair& p) { return (p.first == course); };
 		auto search = std::find_if(assistedCourses.begin(), assistedCourses.end(), criterion);
 		if (search == assistedCourses.end())
 			assistedCourses.push_back(std::make_pair(course, 1));
 		else
 			search->second++;
-		auto descending = [](const DictPair &a, const DictPair &b) {
+		auto descending = [](const DictPair& a, const DictPair& b) {
 			return a.second > b.second;
 		};
 		std::sort(assistedCourses.begin(), assistedCourses.end(), descending);
@@ -84,7 +112,7 @@ struct Course {
 	unsigned short maxTAHours;
 	unsigned short minTACount;
 
-	Course(const std::vector<std::string> &args) {
+	Course(const std::vector<std::string>& args) {
 		courseID = args[0];
 		instructorName = args[1];
 		minTAHours = std::stoi(args[2]);
@@ -93,35 +121,35 @@ struct Course {
 	}
 };
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const* argv[]) {
 	CSVHandler assistantsCSV{"../assistants.csv"};
 	CSVHandler coursesCSV{"../courses.csv"};
-	
+
 	auto assistantData = assistantsCSV.read();
 	auto courseData = coursesCSV.read();
-	
+
 	auto assistants = std::vector<Assistant>();
 	auto courses = std::vector<Course>();
 
 	for (auto dataRow : assistantData)
 		assistants.push_back(Assistant(dataRow));
 
-	auto descending = [](const Assistant &a, const Assistant &b) {
+	auto descending = [](const Assistant& a, const Assistant& b) {
 		return a.assistedCourses.size() > b.assistedCourses.size();
 	};
 	std::sort(assistants.begin(), assistants.end(), descending);
-	
+
 	for (auto assistant : assistants)
 		std::cout << assistant.name << " " << assistant.assistedCourses.size() << std::endl;
 
 	for (auto dataRow : courseData)
 		courses.push_back(Course(dataRow));
 
-	auto coursesDesc = [](const Course &a, const Course &b) {
+	auto coursesDesc = [](const Course& a, const Course& b) {
 		return a.minTAHours > b.minTAHours;
 	};
 	std::sort(courses.begin(), courses.end(), coursesDesc);
-	
+
 	for (auto course : courses)
 		std::cout << course.courseID << " " << course.minTAHours << std::endl;
 
